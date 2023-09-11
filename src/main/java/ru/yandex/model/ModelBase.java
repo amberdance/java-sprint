8291566c61aeea4b32b3e6ccd5c4349bd33b5c93
Alexey Fishler <amberdance@yandex.ru>
2023-09-11 19:10:11 +0300

    fix: Поменял тип id на int

--- src/main/java/ru/yandex/model/ModelBase.java
+++ src/main/java/ru/yandex/model/ModelBase.java
@@ -1,15 +1,14 @@
 package ru.yandex.model;
 
+import lombok.Getter;
+import lombok.Setter;
 import lombok.ToString;
 
-import java.util.UUID;
-
 @ToString
+@Getter
+@Setter
 public class ModelBase {
 
-    protected UUID id;
+    protected int id;
 
-    public ModelBase() {
-        this.id = UUID.randomUUID();
-    }
 }
