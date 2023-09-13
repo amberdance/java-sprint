a7ec048aecc478cb6f8578516cecbcbf908ecc88
Alexey Fishler <amberdance@yandex.ru>
2023-09-13 20:24:11 +0300

    fix: Обновил Main

--- src/main/java/ru/yandex/Main.java
+++ src/main/java/ru/yandex/Main.java
@@ -3,15 +3,29 @@ package ru.yandex;
 import ru.yandex.model.Epic;
 import ru.yandex.model.Subtask;
 import ru.yandex.model.Task;
+import ru.yandex.repository.EpicRepositoryImpl;
+import ru.yandex.repository.SubtaskRepositoryImpl;
+import ru.yandex.repository.TaskRepositoryImpl;
+import ru.yandex.service.TaskServiceImpl;
+import ru.yandex.utils.ArrayListLastIdGenerator;
+
+import java.util.ArrayList;
 
 public class Main {
-    public static void main(String[] args) {
-        var task = new Task("taskname", "taskdescr");
-        var epic = new Epic("epicname", "epicdescr");
-        var subtask = new Subtask("subtaskname", "subtaskdescr");
 
-        System.out.println(task);
-        System.out.println(epic);
-        System.out.println(subtask);
+    //Driver code
+    public static void main(String[] args) {
+        var dataSource = new ArrayList<Task>();
+        var idGenerator = new ArrayListLastIdGenerator(dataSource);
+        var taskRepository = new TaskRepositoryImpl(dataSource, idGenerator);
+        var epicRepository = new EpicRepositoryImpl(dataSource, idGenerator);
+        var subtaskRepository = new SubtaskRepositoryImpl(dataSource, idGenerator);
+        var taskService = new TaskServiceImpl(taskRepository, epicRepository, subtaskRepository);
+
+        taskService.createTask(new Task("taskname", "taskdescr"));
+        taskService.createTask(new Epic("epicname", "epicdescr"));
+        taskService.createTask(new Subtask("subtaskname", "subtaskdescr"));
+
+        System.out.println(taskService.getTasks());
     }
 }
