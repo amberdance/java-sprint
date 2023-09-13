178b24745228fa0db150ac32ec65b55e3cd86562
Alexey Fishler <amberdance@yandex.ru>
2023-09-13 22:39:03 +0300

    refactor: Заменил var на типы для любования красотой синтаксиса

--- src/main/java/ru/yandex/Main.java
+++ src/main/java/ru/yandex/Main.java
@@ -5,22 +5,26 @@ import ru.yandex.model.Subtask;
 import ru.yandex.model.Task;
 import ru.yandex.repository.EpicRepositoryImpl;
 import ru.yandex.repository.SubtaskRepositoryImpl;
+import ru.yandex.repository.TaskRepository;
 import ru.yandex.repository.TaskRepositoryImpl;
+import ru.yandex.service.TaskService;
 import ru.yandex.service.TaskServiceImpl;
 import ru.yandex.utils.ArrayListIdGenerator;
+import ru.yandex.utils.IdGenerator;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
 
     //Driver code
     public static void main(String[] args) {
-        var dataSource = new ArrayList<Task>();
-        var idGenerator = new ArrayListIdGenerator(dataSource);
-        var taskRepository = new TaskRepositoryImpl(dataSource, idGenerator);
-        var epicRepository = new EpicRepositoryImpl(dataSource, idGenerator);
-        var subtaskRepository = new SubtaskRepositoryImpl(dataSource, idGenerator);
-        var taskService = new TaskServiceImpl(taskRepository, epicRepository, subtaskRepository);
+        List<Task> dataSource = new ArrayList<>();
+        IdGenerator idGenerator = new ArrayListIdGenerator(dataSource);
+        TaskRepository<Task> taskRepository = new TaskRepositoryImpl(dataSource, idGenerator);
+        TaskRepository<Epic> epicRepository = new EpicRepositoryImpl(dataSource, idGenerator);
+        TaskRepository<Subtask> subtaskRepository = new SubtaskRepositoryImpl(dataSource, idGenerator);
+        TaskService taskService = new TaskServiceImpl(taskRepository, epicRepository, subtaskRepository);
 
         taskService.createTask(new Task("taskname", "taskdescr"));
         taskService.createTask(new Epic("epicname", "epicdescr"));
