609481a1dc8fad40177d7da34031cdd2197825f7
Alexey Fishler <amberdance@yandex.ru>
2023-09-30 16:42:05 +0300

    fix: Убрал лишнюю аннотацию

--- src/main/java/ru/yandex/managers/InMemoryHistoryManager.java
+++ src/main/java/ru/yandex/managers/InMemoryHistoryManager.java
@@ -1,12 +1,10 @@
 package ru.yandex.managers;
 
-import lombok.RequiredArgsConstructor;
 import org.apache.commons.collections4.queue.CircularFifoQueue;
 import ru.yandex.model.Task;
 
 import java.util.Queue;
 
-@RequiredArgsConstructor
 public class InMemoryHistoryManager implements HistoryManager {
     public static final short HISTORY_CAPACITY = 10;
     private final Queue<Task> history = new CircularFifoQueue<>(HISTORY_CAPACITY);
